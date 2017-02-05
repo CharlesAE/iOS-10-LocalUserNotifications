c46346a2aeaf661e6d3f4deff1bd27c2e06884be
CharlesAE <supa.ram.studios@gmail.com>
2017-02-05 07:25:56 -0500

    Initial Commit

---
+++ LocalUserNotificationDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LocalUserNotificationDemo
+//
+//  Created by Supa Ram on 2/5/17.
+//  Copyright © 2017 LEO Technology. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
